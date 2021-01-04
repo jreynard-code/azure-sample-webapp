c5082652d6be7d0e9144ba585eed171f3f51ca90
Vincent Carluer <vcarluer@gmail.com>
2021-01-04 09:12:45 +0100

    user signin flow

--- src/service/auth.js
+++ src/service/auth.js
@@ -2,9 +2,11 @@ import { Auth, AuthDev } from '@cosmotech/core'
 import { AuthMSAL, AuthStaticWebApp } from '@cosmotech/azure'
 
 // AuthMSAL configuration
+// authority: 'https://login.microsoftonline.com/1fcfc752-2be8-42b2-be24-0f1bb2ef2164/',
+// scopes: ['user.read']
 const msalConfig = {
   loginRequest: {
-    scopes: ['user.read']
+    scopes: ['openid', 'profile']
   },
   accessRequest: {
     scopes: ['https://cosmotechweb.onmicrosoft.com/cosmo_sample/cosmoplatform']
@@ -13,12 +15,30 @@ const msalConfig = {
     auth: {
       clientId: '3ae79982-a3dd-471b-9a9e-268b4ff0d5a6',
       redirectUri: window.location.protocol + '//' + window.location.host + '/digitaltwin',
-      authority: 'https://login.microsoftonline.com/1fcfc752-2be8-42b2-be24-0f1bb2ef2164/',
-      knownAuthorities: ['https://login.microsoftonline.com']
+      authority: 'https://cosmotechweb.b2clogin.com/tfp/cosmotechweb.onmicrosoft.com/B2C_1_SignUp_SignIn',
+      knownAuthorities: ['https://cosmotechweb.b2clogin.com/cosmotechweb.onmicrosoft.com/B2C_1_SignUp_SignIn', 'https://login.microsoftonline.com']
     },
     cache: {
       cacheLocation: 'localStorage',
       storeAuthStateInCookie: true
+    }
+  },
+  b2cPolicies: {
+    names: {
+      signUpSignIn: 'B2C_1_SignUp_SignIn',
+      forgotPassword: 'B2C_1_ResetPassword',
+      editProfile: 'B2C_1_EditProfile'
+    },
+    authorities: {
+      signUpSignIn: {
+        authority: 'https://cosmotechweb.b2clogin.com/cosmotechweb.onmicrosoft.com/B2C_1_SignUp_SignIn'
+      },
+      forgotPassword: {
+        authority: 'https://cosmotechweb.b2clogin.com/cosmotechweb.onmicrosoft.com/B2C_1_ResetPassword'
+      },
+      editProfile: {
+        authority: 'https://cosmotechweb.b2clogin.com/cosmotechweb.onmicrosoft.com/B2C_1_EditProfile'
+      }
     }
   }
 }
