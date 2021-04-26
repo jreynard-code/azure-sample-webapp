6175a23ed2f35216a5d4e197e0da90faed0e05d4
COSMO-VMI <valentin.michelet@cosmotech.com>
2021-04-26 14:24:48 +0200

    WIP

---
+++ src/views/Dashboards/Dashboard.config.js
@@ -0,0 +1,21 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+// Tabs configuration
+export const dashboards = [
+  {
+    id: 0,
+    title: 'Dashboard 1',
+    url: 'https://app.powerbi.com/reportEmbed?reportId=018525c4-3fed-49e7-9048-6d6237e80145&autoAuth=true&ctid=e9641c78-d0d6-4d09-af63-168922724e7f&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWZyYW5jZS1jZW50cmFsLWEtcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D'
+  },
+  {
+    id: 1,
+    title: 'Dashboard 2',
+    url: ''
+  },
+  {
+    id: 2,
+    title: 'Dashboard 3',
+    url: ''
+  }
+];
